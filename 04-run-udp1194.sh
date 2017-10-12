fa202e03b8cb5da3ba6c1694087669ebb34aa3b4
Dave Burke <dburke84@gmail.com>
2017-10-11 20:18:34 -0500

    Run udp1194 as privileged

--- 04-run-udp1194.sh
+++ 04-run-udp1194.sh
@@ -6,5 +6,5 @@ stopContainer ${UDP_CONTAINER}
 removeContainer ${UDP_CONTAINER}
 
 echo "Running container..."
-docker run --volumes-from ${OVPN_DATA} --restart=always --detach --publish 1194:1194/udp --cap-add=NET_ADMIN --name "${UDP_CONTAINER}" ${IMAGE}
+docker run --volumes-from ${OVPN_DATA} --restart=always --detach --publish 1194:1194/udp --cap-add=NET_ADMIN --privileged --name "${UDP_CONTAINER}" ${IMAGE}
 
